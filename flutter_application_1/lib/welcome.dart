b4334abeff6499785be300b7525710b26f7d394c
AmmaraAkhtar <1144ammara@gmail.com>
2026-01-22 14:12:00 +0500

    login page frontend code

--- flutter_application_1/lib/welcome.dart
+++ flutter_application_1/lib/welcome.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'login.dart';
 
 class welcome extends StatefulWidget {
   const welcome({super.key});
@@ -10,21 +11,18 @@ class welcome extends StatefulWidget {
 class _welcomeState extends State<welcome> {
   @override
   Widget build(BuildContext context) {
-     return Scaffold(
-       backgroundColor: Colors.white,
-         body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 24.0),
-          child: Column(
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SizedBox(
+        width:double.infinity,
+        height:double.infinity,
+
+       child:Column(
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              SizedBox(height:30),
-              
-              Image.asset(
-                'assets/logo.jpg', 
-                height: 320,
-              
-              ),
+              SizedBox(height: 30),
+
+              Image.asset('assets/logo.jpg', height: 320),
 
               SizedBox(height: 30),
 
@@ -34,83 +32,84 @@ class _welcomeState extends State<welcome> {
                 style: TextStyle(
                   fontSize: 50,
                   fontWeight: FontWeight.bold,
-                  color: Colors.teal[700],
+                  color: Color(0xFF699886),
                 ),
               ),
 
               SizedBox(height: 30),
 
               RichText(
-              textAlign: TextAlign.center,
-               text: TextSpan(
-               style: TextStyle(
-               fontSize: 25,
-               color: Colors.grey[700],
+                textAlign: TextAlign.center,
+                text: TextSpan(
+                  style: TextStyle(fontSize: 25, color: Colors.grey[700]),
+                  children: <TextSpan>[
+                    TextSpan(text: 'Welcome to the '),
+                    TextSpan(
+                      text: 'The Watcher',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    TextSpan(
+                      text:
+                          ',\nwhere parents can monitor the\n activities of their off springs.',
+                    ),
+                  ],
                 ),
-                children: <TextSpan>[
-                TextSpan(text: 'Welcome to the '),
-                 TextSpan(
-                  text: 'The Watcher',
-                  style: TextStyle(fontWeight: FontWeight.bold),
-                  ),
-                   TextSpan(text: ',\nwhere parents can monitor the\n activities of their off springs.'),
-                ],
-                ),
-                ),
+              ),
               SizedBox(height: 60),
 
               // Login Button
               SizedBox(
-                width: 320,
-                height:55,
-              child:ElevatedButton(
-                onPressed: () {
-                  // Navigate to Login Screen
-                },
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Color(0xFFE59885), 
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(40),
+                width: 274,
+                height: 47,
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => login()),
+                    );
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Color(0xFFEB9974),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(40),
+                    ),
+                  ),
+                  child: Text(
+                    'Login',
+                    style: TextStyle(
+                      fontSize: 22,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
                 ),
-                child: Text(
-                  'Login',
-                  style: TextStyle(
-                    fontSize: 20,
-                    color:Colors.white,
-                     fontWeight: FontWeight.bold),
-                ),
-              ),
               ),
               SizedBox(height: 40),
 
-             
-                SizedBox(
-                width: 320,
-                height:55,
-              child:OutlinedButton(
-                onPressed: () {
-                 
-                },
-                style: OutlinedButton.styleFrom(
-                  side: BorderSide(color: Color(0xFFE59885)),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(40),
+              SizedBox(
+                width: 274,
+                height: 47,
+                child: OutlinedButton(
+                  onPressed: () {},
+                  style: OutlinedButton.styleFrom(
+                    side: BorderSide(color: Color(0xFFEB9974), width:3),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(40),
+                    ),
+                  ),
+                  child: Text(
+                    'Sign Up',
+                    style: TextStyle(
+                      fontSize: 22,
+                      color: Color(0xFFE59885),
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
                 ),
-                child: Text(
-                  'Sign Up',
-                  style: TextStyle(
-                    fontSize: 20,
-                    color: Color(0xFFE59885),
-                    fontWeight: FontWeight.bold
-                  ),
-                ),
-              ),),
+              ),
             ],
           ),
         ),
-      ),
     
     );
   }
