c1322a769162e3f3d98094aa8ef9732ac7e67dfe
AmmaraAkhtar <1144ammara@gmail.com>
2026-01-23 15:34:22 +0500

    Reset Password Frontend

---
+++ flutter_application_1/lib/resetPassword2.dart
@@ -0,0 +1,157 @@
+import 'package:flutter/material.dart';
+
+class Resetpassword2 extends StatefulWidget {
+  const Resetpassword2({super.key});
+
+  @override
+  State<Resetpassword2> createState() => _Resetpassword2State();
+}
+
+class _Resetpassword2State extends State<Resetpassword2> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(250), // AppBar ka height adjust
+        child: AppBar(
+          backgroundColor: Colors.white,
+          elevation: 0,
+          centerTitle: true,
+          flexibleSpace: Padding(
+            padding: const EdgeInsets.only(top: 50), // top space
+            child: Hero(
+              tag: 'applog',
+              child: Image.asset('assets/logo.jpg', width: 189, height: 189),
+            ),
+          ),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 30, vertical: 0),
+          child: SizedBox(
+            width: double.infinity,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Text(
+                  "Reset Password",
+                  style: TextStyle(
+                    fontSize: 36,
+                    color: Color(0xFF699886),
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(height: 40),
+
+                //TextField for email
+                SizedBox(
+                  width: 350,
+                  height: 46,
+                  child: TextField(
+                    decoration: InputDecoration(
+                      hintText: 'Enter Your Password',
+                      hintStyle: TextStyle(
+                        color: Color.fromARGB(
+                          255,
+                          189,
+                          188,
+                          188,
+                        ), 
+                        fontSize: 16,
+                      ),
+
+                      contentPadding: EdgeInsets.symmetric(horizontal: 8),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(7),
+                        borderSide: BorderSide(
+                          color: Color.fromARGB(255, 189, 188, 188),
+                          width: 1.4,
+                        ),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: BorderSide(
+                          color: Color(0xFF147CF4),
+                          width: 2,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(height:20),
+                                
+                                
+            SizedBox(
+                  width: 350,
+                  height: 46,
+                  child: TextField(
+                    decoration: InputDecoration(
+                      hintText: 'Re-enter Your Password',
+                      hintStyle: TextStyle(
+                        color: Color.fromARGB(
+                          255,
+                          189,
+                          188,
+                          188,
+                        ), 
+                        fontSize: 16,
+                      ),
+
+                      contentPadding: EdgeInsets.symmetric(horizontal: 8),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(7),
+                        borderSide: BorderSide(
+                          color: Color.fromARGB(255, 189, 188, 188),
+                          width: 1.4,
+                        ),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: BorderSide(
+                          color: Color(0xFF147CF4),
+                          width: 2,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+
+                SizedBox(height: 60),
+
+                SizedBox(
+                  width: 274,
+                  height: 47,
+                  child: ElevatedButton(
+                    onPressed: () {
+                    
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Color(0xFFEB9974),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(40),
+                      ),
+                    ),
+                    child: Text(
+                      'Reset',
+                      style: TextStyle(
+                        fontSize: 22,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 25),
+
+                SizedBox(height: 50),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
