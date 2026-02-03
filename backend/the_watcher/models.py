95dc2994e56ff4832a7bb56c5100bb5dcbc72ff7
AmnaAli45 <anasahmed.ru6@gmail.com>
2026-02-03 23:26:49 +0500

    Reset password and otp verification apis are created

--- backend/the_watcher/models.py
+++ backend/the_watcher/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.utils import timezone
+from datetime import timedelta
 
 # Create your models here.
+class otp(models.Model):
+    email = models.EmailField(unique=True)
+    code = models.CharField(max_length=4)
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_verified = models.BooleanField(default=False)
+    
+    def is_expired(self):
+        if timezone.now() > self.created_at + timedelta(minutes=10):
+            return True
+
