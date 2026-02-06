6623f90d3f47e301f011d7ab8eaf72a9d7774328
AmnaAli45 <anasahmed.ru6@gmail.com>
2026-02-06 18:21:00 +0500

    Pairing Mechnism of child app and parent app added

--- backend/the_watcher/serializers.py
+++ backend/the_watcher/serializers.py
@@ -25,8 +25,7 @@ class ParentSerializer(serializers.ModelSerializer):
 class OtpSerializer(serializers.Serializer):
     email = serializers.EmailField()
     otp = serializers.CharField(max_length=4)
-    username = serializers.CharField(max_length=150)
-    password = serializers.CharField(max_length=250)
+    
 
     def validate_otp(self, data):
         if len(data) != 4 :
@@ -46,3 +45,33 @@ class PasswordResetSerializer(serializers.Serializer):
         if validate_password(data) is not None:
             raise serializers.ValidationError(validate_password(data))
         return data
+    
+# Log in Serializer
+class LoginSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    password = serializers.CharField(max_length=250)
+
+# Child Serializer
+class ChildSerializer(serializers.Serializer):
+    firstname = serializers.CharField(max_length=100)
+    lastname = serializers.CharField(max_length=100)
+    age = serializers.IntegerField()
+    screen_time_limit = serializers.IntegerField(default=60)  # in minutes
+
+    def validate_age(self, data):
+        if data < 0 or data > 18:
+            raise serializers.ValidationError("Age must be between 0 and 18.")
+        return data
+
+# Pairing Code Serializer
+class PairingCodeSerializer(serializers.Serializer):
+    pairing_code = serializers.CharField(max_length=4)
+
+    def validate_pairing_code(self, data):
+        if len(data) != 4:
+            raise serializers.ValidationError("Pairing code must be a 4-digit number.")
+        elif not data.isdigit():
+            raise serializers.ValidationError("Pairing code must contain only digits.")
+        return data
+    
+    
--- backend/the_watcher/views.py
+++ backend/the_watcher/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from . serializers import ParentSerializer,OtpSerializer,PasswordResetSerializer
+from . serializers import ParentSerializer,OtpSerializer,PasswordResetSerializer,LoginSerializer,ChildSerializer,PairingCodeSerializer
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework_simplejwt.tokens import RefreshToken
@@ -9,6 +9,8 @@ import random
 from . import models
 from django.core.mail import send_mail
 from django.contrib.auth.models import User
+from django.utils import timezone
+from datetime import timedelta
 
 
 
@@ -17,15 +19,17 @@ from django.contrib.auth.models import User
 def signup_api(request):
     serializer = ParentSerializer(data=request.data)
     if serializer.is_valid():
-        send_otp(serializer.validated_data['email'], serializer.validated_data['username'], serializer.validated_data['password'])
+        email = serializer.validated_data['email']
+        username = serializer.validated_data['username']
+        password = serializer.validated_data['password']
         
-        # user = User.objects.create_user(
-        #     username=serializer.validated_data['username'],
-        #     email=serializer.validated_data['email'],
-        #     password=serializer.validated_data['password']
-        # )
-        
-
+        user = User.objects.create_user(
+            username=username,
+            email=email,
+            password=password,
+            is_active=False
+        )
+        send_otp(email)
 
         # refresh = RefreshToken.for_user(user)
         return Response({
@@ -36,23 +40,36 @@ def signup_api(request):
 # Log In API 
 @api_view(['POST'])
 def login_api(request):
-    serializer = ParentSerializer(data=request.data)
+    print("request.body (raw):", request.body)
+    serializer = LoginSerializer(data=request.data)
     if serializer.is_valid():
         email = serializer.validated_data['email']
         password = serializer.validated_data['password']
-        user = authenticate(request, email=email, password=password)
-        if user is not None:
-            refresh = RefreshToken.for_user(user)
+        user = User.objects.filter(email = email).first()
+        print("User found:", user)
+        if user is None:
+            return Response({"error": "User Not Found"}, status=status.HTTP_404_NOT_FOUND)
+        if not user.is_active:
+            return Response({"error": "Account is not active. Please verify your email."}, status=status.HTTP_403_FORBIDDEN)
+        authenticated_user = authenticate(request, username=user.username, password=password)
+        print("Authenticated user:", authenticated_user)
+        if authenticated_user is not None:
+            refresh = RefreshToken.for_user(authenticated_user)
+            
+                
+            
             return Response({
-                "user": {
-                    "username": user.username,
-                    "email": user.email
-                },
-                "tokens": {
-                    "refresh": str(refresh),
-                    "access": str(refresh.access_token),
-                }
-            }, status=status.HTTP_200_OK)
+                    "user": {
+                        "username": authenticated_user.username,
+                        "email": authenticated_user.email
+                    },
+                    "tokens": {
+                        "refresh": str(refresh),
+                        "access": str(refresh.access_token),
+                    }
+                }, status=status.HTTP_200_OK)
+         
+    print(serializer.errors)
     return Response({"error": "Invalid credentials"}, status=status.HTTP_401_UNAUTHORIZED)
 
 # Email Check API
@@ -97,23 +114,24 @@ def generate_otp():
 
 # Function for sending otp to email
 
-def send_otp(email,username,password):
+def send_otp(email):
     # serializer = ParentSerializer(data=request.data)
     # email = serializer.validated_data['email']
     try:
         # user = User.objects.get(email=email)
         otp = generate_otp()
         print (otp)
-        models.otp.objects.create(email=email,code=otp,username=username,password=password)
+        models.otp.objects.create(email=email,code=otp)
         print("Created")
-        #send_mail(
-            #subject='OTP for TheWatcher App',
-            #message=f'Your OTP code is {otp}',
-            #from_email='22ntucs1145amnaali@gmail.com',
-            #recipient_list=[email],
-            #fail_silently=False
+        send_mail(
+
+            subject='OTP for TheWatcher App',
+            message=f'Your OTP code is {otp}',
+            from_email='22ntucs1145amnaali@gmail.com',
+            recipient_list=[email],
+            fail_silently=False
            
-        #)
+        )
     except Exception as e:
             print("Error:", e)
             return Response({"error": f"Error sending OTP: {e}"}, status=status.HTTP_400_BAD_REQUEST)
@@ -128,10 +146,10 @@ def otpVerification_api(request):
     serializer = OtpSerializer(data=request.data)
     if serializer.is_valid():
         print("Serializer valid:", serializer.validated_data)
-       #email = serializer.validated_data['email']
+        email = serializer.validated_data['email']
         code = serializer.validated_data['otp']
         
-        otp_record = models.otp.objects.filter( code=code,is_verified=False).last()
+        otp_record = models.otp.objects.filter( email=email,code=code,is_verified=False).last()
         if not otp_record:
             return Response({"result": "Invalid OTP"}, status=status.HTTP_400_BAD_REQUEST)
         if otp_record.is_expired():
@@ -139,11 +157,13 @@ def otpVerification_api(request):
         otp_record.is_verified = True
         otp_record.save()
 
-        user = User.objects.create_user(
-            username=otp_record.username,
-            email=otp_record.email,
-            password=otp_record.password
-        )
+        user = User.objects.get(email=email)
+        user.is_active = True
+        user.save()
+
+        otp_record.delete() 
+
+       
         refresh = RefreshToken.for_user(user)
         return Response({
                 "user": {
@@ -158,4 +178,105 @@ def otpVerification_api(request):
   
     print(serializer.errors)
     return Response({"errors": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+
+# resend otp API
+@api_view(['POST'])
+def resendOtp_api(request):
+    email = request.data.get('email')
+    send_otp(email)
+    return Response({"message": "OTP resent to your email."}, status=status.HTTP_200_OK)
+
+# Create Child API
+@api_view(['POST'])
+def createChild_api(request):
+    delete_unpaired_children()
+    serializer = ChildSerializer(data=request.data)
+    if serializer.is_valid():
+        firstname = serializer.validated_data['firstname']
+        lastname = serializer.validated_data['lastname']
+        age = serializer.validated_data['age']
+        screen_time_limit = serializer.validated_data['screen_time_limit']
+        parent_email = request.data.get('parent_email')
+        user = User.objects.filter(email=parent_email).first()
+        pairing_code = generate_unique_code()
+        print(pairing_code)
+        if models.child.objects.filter(firstname=firstname,lastname=lastname,parent=user).exists():
+            return Response({"error": "Child already exists for this parent"}, status=400)
+        child = models.child.objects.create(firstname=firstname, lastname=lastname, age=age, screen_time_limit=screen_time_limit,parent = user,pairingCode=pairing_code)
+        models.pairingCode.objects.create(pairing_code=pairing_code,parent=user,child=child)    
+        send_code(parent_email, pairing_code)
+        return Response({
+            "message": "Child created successfully",
+           
+        }, status=status.HTTP_201_CREATED)
+    return Response({"errors": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+
+# Generate Unique code 
+def generate_unique_code():
+        while True:
+            code = str(random.randint(1000, 9999))
+            if not models.pairingCode.objects.filter(pairing_code=code).exists():
+                return code
+
+# Function for sending pairing code  to parent's email
+
+def send_code(email,code):
+    
+    try:
+
+       
         
+       
+        print("Created")
+        send_mail(
+
+            subject='OTP for TheWatcher App',
+            message=f'Your OTP code is {code}',
+            from_email='22ntucs1145amnaali@gmail.com',
+            recipient_list=[email],
+            fail_silently=False
+           
+        )
+    except Exception as e:
+            print("Error:", e)
+            return Response({"error": f"Error sending OTP: {e}"}, status=status.HTTP_400_BAD_REQUEST)
+
+# pairing code verification API
+@api_view(['POST'])
+def pairingCodeVerification_api(request):
+    serializer = PairingCodeSerializer(data=request.data)
+    if not serializer.is_valid():
+        print("Serializer errors:", serializer.errors)
+        return Response({"error": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+    pairing_code = serializer.validated_data['pairing_code']
+    try:
+        print("Request pairing code:", pairing_code)
+        print("Available unused codes:", list(models.pairingCode.objects.filter(is_used=False).values_list('pairing_code', flat=True)))
+        code_record = models.pairingCode.objects.filter(pairing_code=pairing_code,is_used=False).order_by('-created_at').first()
+        if not code_record:
+            print("Invalid pairing code")
+            return Response({"error": "Invalid Pairing Code"}, status=status.HTTP_400_BAD_REQUEST)
+        if code_record.is_expired():
+            print("Pairing code expired")
+            return Response({"error": "Pairing Code has expired"}, status=status.HTTP_400_BAD_REQUEST)
+        code_record.is_used = True
+        code_record.save()
+
+        child = models.child.objects.filter(pairingCode=pairing_code).first()
+        if child:
+            child.is_paired = True
+            child.save()
+
+        return Response({"message": "Pairing successful"}, status=status.HTTP_200_OK)
+    except Exception as e:
+        print("Error during pairing:", e)
+        return Response({"error": "Error Occured"}, status=status.HTTP_400_BAD_REQUEST)
+
+
+# Delete Unpaired Children 
+def delete_unpaired_children():
+    expire_time = timezone.now() - timedelta(minutes=30)
+    expired_codes = models.pairingCode.objects.filter(is_used=False, created_at__lte=expire_time)
+    for code in expired_codes:
+        models.child.objects.filter(pairingCode=code).delete()
+        code.delete()
