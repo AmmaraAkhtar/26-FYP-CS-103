667d47e164e604d12961188f58265893d8df9d1f
AmnaAli45 <anasahmed.ru6@gmail.com>
2026-04-30 15:34:23 +0500

    App Usage table updated

--- backend/the_watcher/models.py
+++ backend/the_watcher/models.py
@@ -45,6 +45,9 @@ class appUsage(models.Model):
     usage_time = models.IntegerField(default = 0) 
     package_name = models.CharField(null=True,max_length = 255)
     created_at = models.DateTimeField(auto_now_add=True)
+    risk = models.CharField(max_length=20)
+    action = models.CharField(max_length=20)
+    category = models.CharField(max_length=50)
     date = models.DateField()
 
 # Alerts Model
