93b2451de72357ad25dc73b19b8f722b4e8dc7bd
AmnaAli45 <anasahmed.ru6@gmail.com>
2026-05-25 16:10:31 +0500

    build.gradle file updated

--- child_app/android/app/build.gradle.kts
+++ child_app/android/app/build.gradle.kts
@@ -40,8 +40,9 @@ android {
     }
 }
 dependencies {
-
+    
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
 
 }
 
